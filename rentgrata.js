59fd34abe01f6d124909d48d195c6662864ec46a
Kenny Hsieh <kennybacktoschool@gmail.com>
2021-12-03 14:48:01 -0800

    program complete

---
+++ rentgrata.js
@@ -0,0 +1,97 @@
+"use strict";
+
+const fs = require("fs");
+const { eventNames } = require("process");
+
+// pull data from files
+const eventsData = fs.readFileSync("events.json");
+const events = JSON.parse(eventsData);
+const usersData = fs.readFileSync("users.json");
+const users = JSON.parse(usersData);
+
+// returns array of arguments when rentgrata.js is called in commandline.
+const args = process.argv.slice(2);
+
+// returns array of id of corresponding user arguments
+const usersArrayById = [];
+
+for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < args.length; j++) {
+        if (users[i].name === args[j]) {
+            usersArrayById.push(users[i].id);
+        }
+    }
+}
+
+// returns all events with user_id matches usersArrayById and date changed to ISO standard in UTC time.
+let allEventsFromInputUsers = [];
+for (let i = 0; i < events.length; i++) {
+    for (let j = 0; j < usersArrayById.length; j++) {
+        if (events[i].user_id === usersArrayById[j]) {
+            events[i].start_time = new Date(`${events[i].start_time}Z`);
+            events[i].end_time = new Date(`${events[i].end_time}Z`);
+            allEventsFromInputUsers.push(events[i]);
+        }
+    }
+}
+
+// sorts allEventsFromInputUsers array by date and time in ascending order
+allEventsFromInputUsers.sort((a, b) => {
+    return a.start_time - b.start_time;
+});
+
+// add beginning and end times and dates. Find all void times between the constraints and list onto array
+let startOfDayTime = 13;
+let endOfDayTime = 21;
+let year = 2021;
+const monthIndex = 6;
+let startDate = 5;
+let endDate = 7;
+
+// assign events into Array where each index is an array of events sorted by Date.
+const eventsFilteredByUserAndDate = [];
+for (let date = startDate; date <= endDate; date++) {
+    eventsFilteredByUserAndDate.push(allEventsFromInputUsers.filter((event) => event.start_time.getUTCDate() == date));
+}
+
+// format time string to be two digits when less than two digits.
+const formatTime = (time) => {
+    return ("0" + time).slice(-2);
+};
+
+// format strings for output.
+const formatTimeStringForOutput = (time1, time2) => {
+    return `${year}-0${monthIndex + 1}-0${time1.getUTCDate()} ${formatTime(time2.getUTCHours())}:${formatTime(
+        time2.getUTCMinutes()
+    )} - ${formatTime(time1.getUTCHours())}:${formatTime(time1.getUTCMinutes())}`;
+};
+
+// map through each index and find availability gaps
+const availableTimes = [];
+eventsFilteredByUserAndDate.map((event) => {
+    let startTimeOfTheDay = new Date(Date.UTC(year, monthIndex, event[0].start_time.getUTCDate(), startOfDayTime));
+    let endOfDateTime = new Date(Date.UTC(year, monthIndex, event[0].start_time.getUTCDate(), endOfDayTime));
+    let currentTime = startTimeOfTheDay;
+    for (let i = 0; i < event.length; i++) {
+        // first scenario where i === 0 and there is gap betwen start of day and first event
+        if (i === 0 && event[i].start_time > currentTime) {
+            availableTimes.push(formatTimeStringForOutput(event[i].start_time, currentTime));
+            // second scenario to check if there is availability after last event of the day
+        } else if (i === event.length - 1) {
+            if (event[i].start_time > currentTime) {
+                availableTimes.push(formatTimeStringForOutput(event[i].start_time, currentTime));
+            }
+            if (event[i].end_time < endOfDateTime) {
+                availableTimes.push(formatTimeStringForOutput(endOfDateTime, event[i].end_time));
+            }
+            // if there are availability between events
+        } else if (i !== event.length - 1 && event[i].start_time > currentTime) {
+            availableTimes.push(formatTimeStringForOutput(event[i].start_time, currentTime));
+        } else {
+        }
+        currentTime = currentTime > event[i].end_time ? currentTime : event[i].end_time;
+    }
+    availableTimes.push(" ");
+});
+
+console.log(availableTimes.join(" \n"));
